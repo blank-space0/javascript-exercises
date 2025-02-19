469071374e04d3e2ae7890e8fd4ac8547b3d4121
alexs <lightserna171@gmail.com>
2025-02-18 21:23:00 -0600

    Completed more Excercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(lower, upper) {
+    if (!Number.isInteger(lower) || !Number.isInteger(upper)
+        || lower < 0 || upper < 0) {
+        return 'ERROR';
+    }
+    if (upper < lower) {
+        var temp = upper;
+        upper = lower;
+        lower = temp;
+    }
 
+    let ans = 0
+    while (lower <= upper) {
+        ans += lower;
+        lower += 1
+    }
+
+    return ans;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(curr) {
+  var ans = (curr - 32) * (5/9)
+  return Math.round(ans * 1e1) / 1e1;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(curr) {
+  var ans = (curr * (9/5) + 32)
+  return Math.round(ans * 1e1 ) / 1e1;
 };
 
 // Do not edit below this line
